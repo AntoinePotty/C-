802624eb73ee2554b6e9269bd8bd3d8495ecee08
AntoinePotty <114071689+AntoinePotty@users.noreply.github.com>
2022-10-25 11:10:54 +0200

    Add files via upload

---
+++ Etape04/Classe/Employe.h
@@ -0,0 +1,42 @@
+#ifndef EMPLOYE_H
+#define EMPLOYE_H
+#include <string>
+#include "Intervenant.h"
+
+using namespace std; 
+
+class Employe : public Intervenant
+{
+	friend ostream& operator<<(ostream&,const Employe&);
+
+protected:
+	string login;
+	string* motDePasse = NULL;
+	string fonction;
+
+
+public:
+	static string ADMINISTRATIF;
+	static string VENDEUR;
+
+	Employe();
+	Employe(string,string,int,string,string);
+	Employe(const Employe&);
+	~Employe();
+
+	Employe& operator=(const Employe&);
+
+	string ToString()const;
+	string Tuple()const;
+
+	void setLogin(string);
+	string getLogin()const;
+
+	void setMotDePasse(string);
+	string getMotDePasse()const;
+	void ResetMotDePasse(string*);
+
+	int setFonction(string);
+	string getFonction()const;
+};
+#endif
